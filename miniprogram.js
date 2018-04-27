540686bfb170c6b1e7d6dfdfeeb818b2e0cbc0aa
JingkaiZhao <jingkai.zhao@foxmail.com>
2018-04-27 15:06:52 +0800

    feat: add miniprogram plugin

--- miniprogram.js
+++ miniprogram.js
@@ -1,9 +1,31 @@
-module.exports = class MpvueExtraPlugin {
-  constructor() {
+const path = require('path')
+const fs = require('fs')
 
+const genAsset = (code) => ({
+  source() {
+    return code
+  },
+  size() {
+    return code.length
   }
+})
 
-  apply() {
-
+module.exports = class MpvueExtraPlugin {
+  apply(compiler) {
+    compiler.plugin('emit', (compilation, next) => {
+      const pageReg = /pages\/.+\/.+/i
+      const pageChunks = compilation.chunks.filter(c => pageReg.test(c.name))
+      pageChunks.forEach(pc => {
+        const entryResource = pc.entryModule.resource
+        const info = path.parse(entryResource)
+        const jsonFile = path.join(info.dir, info.name + '.json')
+        let content = JSON.stringify({})
+        if (fs.existsSync(jsonFile)) {
+          content = JSON.stringify(require(jsonFile))
+        }
+        compilation.assets[`${pc.name}.json`] = genAsset(content)
+      })
+      next()
+    })
   }
 }
